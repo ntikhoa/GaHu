3c32932ab0711bedd6008a8419b1d6bc83856c9a
Khoa <ntikhoa123@gmail.com>
2021-09-26 16:08:40 +0700

    Update app.js

--- Backend/app.js
+++ Backend/app.js
@@ -51,6 +51,8 @@ app.use((err, req, res, next) => {
     });
 });
 
+console.log(process.env.MONGO_USERNAME);
+
 mongoose.connect(`mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0.pv0tn.mongodb.net/${process.env.MONGO_DB_NAME}?retryWrites=true&w=majority`,
     {
         useNewUrlParser: true,
