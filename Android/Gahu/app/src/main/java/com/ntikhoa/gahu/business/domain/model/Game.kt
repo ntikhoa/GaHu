c5153bb9b05bd4aa67d0e55e376a1471d57d46bb
Khoa <ntikhoa123@gmail.com>
2022-01-15 10:11:36 +0700

    game adapter

--- Android/Gahu/app/src/main/java/com/ntikhoa/gahu/business/domain/model/Game.kt
+++ Android/Gahu/app/src/main/java/com/ntikhoa/gahu/business/domain/model/Game.kt
@@ -10,4 +10,15 @@ open class Game(
     val imageUrl: String,
     val description: String,
     val author: Author
-)
+) {
+    override fun equals(other: Any?): Boolean {
+        if (other is Game)
+            return (id == other.id
+                    && title == other.title
+                    && releaseDate == other.releaseDate
+                    && imageUrl == other.imageUrl
+                    && description == other.description
+                    && author == other.author)
+        else return false
+    }
+}
---
+++ Android/Gahu/app/src/main/java/com/ntikhoa/gahu/presentation/game/adapter/GameAdapter.kt
@@ -0,0 +1,82 @@
+package com.ntikhoa.gahu.presentation.game.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.*
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
+import com.ntikhoa.gahu.R
+import com.ntikhoa.gahu.business.domain.model.Game
+import com.ntikhoa.gahu.databinding.ItemGameBinding
+
+class GameAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+    private val DIFF_CALLBACK = object : DiffUtil.ItemCallback<Game>() {
+        override fun areItemsTheSame(oldItem: Game, newItem: Game): Boolean {
+            return oldItem.id == newItem.id
+        }
+
+        override fun areContentsTheSame(oldItem: Game, newItem: Game): Boolean {
+            return oldItem == newItem
+        }
+
+    }
+
+    private val differ = AsyncListDiffer(
+        GameRecyclerChangeCallBack(this),
+        AsyncDifferConfig.Builder(DIFF_CALLBACK).build()
+    )
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val view = inflater.inflate(R.layout.item_game, parent, false)
+        val binding = ItemGameBinding.bind(view)
+        return GameViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        val game = differ.currentList[position]
+        when (holder) {
+            is GameViewHolder -> holder.bind(game)
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return differ.currentList.size
+    }
+
+    class GameViewHolder(private val binding: ItemGameBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(game: Game) {
+            binding.apply {
+                Glide.with(root)
+                    //.setDefaultRequestOptions(requestOptions) //for cache image
+                    .load(game.imageUrl)
+                    .transition(DrawableTransitionOptions.withCrossFade())
+                    .into(ivGameImage)
+
+                tvGameTitle.text = game.title
+            }
+        }
+    }
+
+    internal inner class GameRecyclerChangeCallBack(
+        private val adapter: GameAdapter
+    ) : ListUpdateCallback {
+        override fun onInserted(position: Int, count: Int) {
+            adapter.notifyItemRangeChanged(position, count)
+        }
+
+        override fun onRemoved(position: Int, count: Int) {
+            adapter.notifyDataSetChanged()
+        }
+
+        override fun onMoved(fromPosition: Int, toPosition: Int) {
+            adapter.notifyDataSetChanged()
+        }
+
+        override fun onChanged(position: Int, count: Int, payload: Any?) {
+            adapter.notifyItemRangeChanged(position, count, payload)
+        }
+    }
+}
